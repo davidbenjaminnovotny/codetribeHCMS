61769713ff65a441dc8c7d54cf4f445456acaf06
davidbenjaminnovotny <novotnydavid3@gmail.com>
2020-04-15 16:02:58 +0200

    base config set

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,16 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
-
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'CodeTribe',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'articles/**/*.md',
+        typeName: 'Articles',
+        remark: {}
+      }
+    },
+  ],
+  transformers: {
+    remark: {}
+  }
 }
